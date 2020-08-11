f2f8fb809e51a9fb7a092bfe98b44ad21ce43ce8
x300n <houseofxeon@gmail.com>
2020-08-12 00:04:36 +0200

    Add /users

---
+++ src/index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+const users = []
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/users', (req, res) => {
+
+  res.send(users)
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
