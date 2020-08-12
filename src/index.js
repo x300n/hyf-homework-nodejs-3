e29a31acec481e058126fb28509d9a4037b77a9b
x300n <houseofxeon@gmail.com>
2020-08-12 10:57:47 +0200

    Add /POST /GET

--- src/index.js
+++ src/index.js
@@ -1,18 +1,38 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
 
-const users = []
+const bodyPraser = require("body-parser");
+app.use(bodyPraser.json());
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const port = 3000;
+let users = [];
+let id = 0;
 
-app.get('/users', (req, res) => {
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
-  res.send(users)
-})
+app.get("/users", (req, res) => {
+  res.send(users);
+});
+
+app.get("/user/:id", (req, res) => {
+  let user = users.filter((user) => user.id == req.params.id);
+  res.send(user[0]);
+});
+
+app.post("/user", (req, res) => {
+  let user = {
+    id: id,
+  };
+
+  id = id + 1;
+
+  users.push(user);
+
+  res.status(200).send(user);
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+  console.log(`Example app listening at http://localhost:${port}`);
+});
